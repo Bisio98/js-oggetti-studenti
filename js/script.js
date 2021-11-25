4a5b6b8255b2e06fdf69456d635a4f093a0ff0ea
Bisio98 <alberto.bisetti@hotmail.it>
2021-11-25 14:48:54 +0100

    completed

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let studenti = [
+    {
+        name: 'Alberto',
+        surname: 'Bisetti',
+        age: '23'
+    },
+    {
+        name: 'Alessandro',
+        surname: 'Scolozzi',
+        age: '33'
+    }
+];
+
+
+for(let i = 0; i < 2; i++){
+    let newStudent = {
+        name: 'Alberto',
+        surname: 'Bisetti',
+        age: '23' 
+    }
+    newStudent.name = prompt('Inserire nome:')
+    newStudent.surname = prompt('Inserire cognome:')
+    newStudent.age = prompt('Inserire età:')
+    studenti.push(newStudent);
+}
+
+for( let i = 0; i < studenti.length; i++ ) {
+    const student = studenti[i];
+    for(let key in student) {
+        console.log(student[key]);
+    }
+}
